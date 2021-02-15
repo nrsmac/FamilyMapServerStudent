1a13f15665c09c3b5a5313c3305c3bbfe1f56d69
nrsmac <noahrschill@gmail.com>
2021-02-15 15:02:21 -0700

    some more stubs

--- src/main/java/JSONParser.java
+++ src/main/java/JSONParser.java
@@ -1,3 +1,36 @@
-public class JSONParser {
+import Model.Model;
+import Model.User;
+import Model.Person;
+import Model.Event;
+import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
+
+public class JSONParser {
+    private Model model;
+    private ArrayList<User> users;
+    private ArrayList<Person> persons;
+    private ArrayList<Event> events;
+
+    private JsonObject json;
+
+    public JSONParser(Model model){
+        this.model = model;
+        parseModel();
+        json = new JsonObject();
+    }
+
+
+
+    /**
+     * Parses the member variable model into member ArrayLists of Users, Persons, and Events
+     */
+    private void parseModel() {
+
+    }
+
+    public JsonObject getJson(){
+        //TODO: Watch gson lecture video. What should we be returning?
+        return this.json;
+    }
 }
